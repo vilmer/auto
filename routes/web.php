3a36a33bfacbfe0f1938dbd3fa8c8d9475afff78
soy-software <soysoftware@gmail.com>
2019-04-23 21:11:46 -0500

    end con

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 Route::get('/', function () {
     /*Artisan::call('cache:clear');
     Artisan::call('config:clear');
-    Artisan::call('config:cache');*/
+    Artisan::call('config:cache');
+    $exitCode = Artisan::call('storage:link', [] );*/
     return view('welcome');
 });
 
